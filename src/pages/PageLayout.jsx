42ec5c792f6fd7508691c4a9d505c8de20113c37
frankbaez77 <franciscobaez100@gmail.com>
2025-02-22 22:37:00 -0400

    agregando archivos para el react router, creacion de home y despliegue de detalles

---
+++ src/pages/PageLayout.jsx
@@ -0,0 +1,32 @@
+import { useRoutes } from "react-router-dom";
+import { Layout } from "./../layout";
+import { Product } from "./Product";
+import { Details } from "./Details";
+import { Home } from "./Home";
+
+export function PageRouter() {
+  let routes = [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "/product", element: <Product /> },
+        {
+          path: "/detail/:id",
+          element: <Details />,
+        },
+        { path: "*", element: <>Pagina no encontrada</> },
+      ],
+    },
+  ];
+
+  // The useRoutes() hook allows you to define your routes as JavaScript objects
+  // instead of <Routes> and <Route> elements. This is really just a style
+  // preference for those who prefer to not use JSX for their routes config.
+  const element = useRoutes(routes);
+
+  return <>{element}</>;
+}
+
+export default PageRouter;
